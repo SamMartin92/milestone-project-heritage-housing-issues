7f7fffbfb6b2fa9789a52fab878cc1a0d7996d86
SamMartin92 <samm92@live.ie>
2022-11-18 22:41:59 +0000

    adds code to predict sale price of inherited

--- app_pages/page_house_price_prediction.py
+++ app_pages/page_house_price_prediction.py
@@ -6,43 +6,86 @@ from src.machine_learning.predict_sale_price_ui import predict_sale_price
 
 def page_house_price_prediction_body():
     version = 'v1'
-    df = load_housing_data()
-    percentageMin, percentageMax = 0.4, 2.0
-
-    fetaures = (pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/{version}/X_train.csv")
+    df_inherited = load_inherited_houses_data()
+    pipeline_regressor = load_pkl_file("outputs/ml_pipeline/predict_saleprice/v1/pipeline_regressor.pkl")
+    features = (pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/{version}/X_train.csv")
 					.columns
 					.to_list()
 					)
     
-    
 
-    for i in range(len(fetaures)):
+
+
+
+    X_live = DrawInputWidgets(version, features)
+    sale_price_prediction = predict_sale_price(X_live, features, pipeline_regressor)
+
+
+
+    
+    st.write(sale_price_prediction)
+
+
+    st.write(df_inherited.iloc[[0]])
+
+    sale_price_prediction_1 =predict_sale_price(df_inherited.iloc[[0]], features, pipeline_regressor)
+
+    st.write(sale_price_prediction_1)
+
+
+    
         
 
 
 
-# 2ndFlrSF,GarageArea,OverallQual,TotalBsmtSF
-
-def DrawInputWidgets():
+def DrawInputWidgets(version, features):
 
     df = load_housing_data()
     percentageMin, percentageMax = 0.4, 2.0
-
-    fetaures = (pd.read_csv(f"outputs/ml_pipeline/predict_churn/{version}/X_train.csv")
-					.columns
-					.to_list()
-					)
     
-    
+    col1, col2, col3, col4 = st.beta_columns(4)
 
-    for i in range(len(fetaures)):
-        print(1)
+    X_live = pd.DataFrame([], index=[0])
 
+    with col1:
+        feature = features[0]
+        st_widget = st.number_input(
+            label = feature,
+            min_value= df[feature].min()*percentageMin,
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median()
+        )
+    X_live[feature] = st_widget
 
-    #     with col1:
-    #         feature = '2ndFlrSF'
-    #         st_widget = st.number_input(
-    #             label = feature,
-    #             options=df[feature].unique()
-    #         )
-    #     X_live[feature] = st_widget
+    with col2:
+        feature = features[1]
+        st_widget = st.number_input(
+            label = feature,
+            min_value= df[feature].min()*percentageMin,
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    with col3:
+        feature = features[2]
+        st_widget = st.number_input(
+            label = feature,
+            min_value= df[feature].min()*percentageMin,
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    with col4:
+        feature = features[3]
+        st_widget = st.number_input(
+            label = feature,
+            min_value= df[feature].min()*percentageMin,
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    return X_live
+
--- src/machine_learning/predict_sale_price_ui.py
+++ src/machine_learning/predict_sale_price_ui.py
@@ -4,6 +4,7 @@ def predict_sale_price(X_live,features, pipeline):
 
     X_live_features = X_live.filter(features)
 
-    prediction = pipeline.predict(X_live_features)
+    prediction = float(pipeline.predict(X_live_features).round(2))
 
-    st.write(prediction)
+    return prediction
+
