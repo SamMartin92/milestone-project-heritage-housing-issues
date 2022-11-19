2b03409274277c8a707465c4307f67accb15e637
SamMartin92 <samm92@live.ie>
2022-11-19 18:02:28 +0000

    completes page_house_price_prediction_body

--- app_pages/page_house_price_prediction.py
+++ app_pages/page_house_price_prediction.py
@@ -12,35 +12,57 @@ def page_house_price_prediction_body():
 					.columns
 					.to_list()
 					)
+
+    
+    sale_price_prediction_1 = predict_sale_price(df_inherited.iloc[[0]], features, pipeline_regressor)
+    sale_price_prediction_2 = predict_sale_price(df_inherited.iloc[[1]], features, pipeline_regressor)
+    sale_price_prediction_3 = predict_sale_price(df_inherited.iloc[[2]], features, pipeline_regressor)
+    sale_price_prediction_4 = predict_sale_price(df_inherited.iloc[[3]], features, pipeline_regressor)
+    
     
 
+    st.write(" # Sale Price Prediction:")
 
+    st.info(
+        """
+        * Our client is interested in predicting the potential sale price of her four inherited properties
+        and would like to be able to predict the sale price of other homes in Ames, Iowa, should she wish 
+         to buy there in the future.
+        * Below are the features and predicted sales prices of her 4 inherited properties:
+        """ 
+        )
+    
+    st.write(df_inherited)
 
+    st.write(
+        f"* **House 1**: Precited Sale Price: ${sale_price_prediction_1}\n"
+        f"* **House 2**: Precited Sale Price: ${sale_price_prediction_2}\n"
+        f"* **House 3**: Precited Sale Price: ${sale_price_prediction_3}\n"
+        f"* **House 4**: Precited Sale Price: ${sale_price_prediction_4}\n"
+        f"\n\n"
+        f"**Total Expected sale price**: "
+        f"${sale_price_prediction_1 + sale_price_prediction_2 + sale_price_prediction_3 + sale_price_prediction_4}"
+    )
+
+    st.info(
+        """
+        * With the below interface, the client can predict the sale price of any home in Ames, Iowa by inputting the 
+        the relevant values and running predictive analysis by clicking the 'Predict Sale Price' button.
+        """
+    )
 
     X_live = DrawInputWidgets(version, features)
     sale_price_prediction = predict_sale_price(X_live, features, pipeline_regressor)
 
-
-
-    
-    st.write(sale_price_prediction)
-
-
-    st.write(df_inherited.iloc[[0]])
-
-    sale_price_prediction_1 =predict_sale_price(df_inherited.iloc[[0]], features, pipeline_regressor)
-
-    st.write(sale_price_prediction_1)
-
-
-    
+    if st.button("Predict Sale Price"):
+        st.write(
+            f"${sale_price_prediction}"
+        )
         
-
-
-
 def DrawInputWidgets(version, features):
 
     df = load_housing_data()
+
     percentageMin, percentageMax = 0.4, 2.0
     
     col1, col2, col3, col4 = st.beta_columns(4)
