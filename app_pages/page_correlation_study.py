6372c8ad7fc35d75dd676d30143d799d8f28d402
SamMartin92 <samm92@live.ie>
2022-11-23 00:19:39 +0000

    adds plot_line to page_correlation_study.py

--- app_pages/page_correlation_study.py
+++ app_pages/page_correlation_study.py
@@ -1,3 +1,4 @@
+import ppscore as pps
 from feature_engine.discretisation import EqualFrequencyDiscretiser
 from feature_engine.imputation import CategoricalImputer
 import streamlit as st
@@ -6,10 +7,6 @@ from src.data_management.data import load_housing_data
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid")
-import ppscore as pps
-
-
-
 
 
 def page_correlation_study():
@@ -28,8 +25,6 @@ def page_correlation_study():
     }
 
     conclusions = print_conclusions(cols_and_conclusions)
-
-    
 
     st.write("## Sale Price Correlation Study")
     st.info(
@@ -62,7 +57,7 @@ def page_correlation_study():
     st.write(
         """
         * Insight into these attributes and their values plotted against sales prices can be seen below.
-            * Each conclusion below maps to its respective plot which can be chosen by selecting the feature 
+            * Each conclusion below maps to respective plots which can be chosen by selecting the feature 
             from the drop down menu.
             * All sale prices have been sorted into 6 ranges for a more general representation of the type
              of sale price one might expect for a property with a given attribute.
@@ -74,9 +69,9 @@ def page_correlation_study():
         f"{print_conclusions(cols_and_conclusions)}"
     )
 
-    load_plots_and_conclusions(df,cols_and_conclusions,plot_numerical)
+    load_plots_and_conclusions(df, cols_and_conclusions, plot_numerical)
 
-    st.info( """
+    st.info("""
         * During feature engineering, heatmaps were produced to study the correlation levels and PPS 
         between all variables in our dataset.
         * Heatmaps are available to view below showing only variables whose correlation levels (Pearson &
@@ -86,17 +81,19 @@ def page_correlation_study():
         """)
 
     if st.checkbox("Pearson Correlation"):
-        heatmap_corr(df=df_corr_pearson,threshold=0.4, figsize=(20,14), font_annot = 14)
+        heatmap_corr(df=df_corr_pearson, threshold=0.4,
+                     figsize=(20, 14), font_annot=14)
 
-    if st.checkbox("Spearman Correlation"):    
-        heatmap_corr(df=df_corr_spearman,threshold=0.4, figsize=(20,14), font_annot = 14)
+    if st.checkbox("Spearman Correlation"):
+        heatmap_corr(df=df_corr_spearman, threshold=0.4,
+                     figsize=(20, 14), font_annot=14)
 
-    if st.checkbox("Predictive Power Score"): 
-        heatmap_pps(df=pps_matrix,threshold=0.2, figsize=(20,14), font_annot = 14)
-    
+    if st.checkbox("Predictive Power Score"):
+        heatmap_pps(df=pps_matrix, threshold=0.2,
+                    figsize=(20, 14), font_annot=14)
 
 
-def load_plots_and_conclusions(df,cols_and_conclusions,plot_numerical):
+def load_plots_and_conclusions(df, cols_and_conclusions, plot_numerical):
     df_disc, hue_order = sale_price_discretisation(df)
     variables = list(cols_and_conclusions.keys())
 
@@ -107,49 +104,60 @@ def load_plots_and_conclusions(df,cols_and_conclusions,plot_numerical):
 
     for variable in variables:
         if option == variable:
-            plot_numerical(df_disc, variable, 'SalePrice', hue_order)
+            if st.checkbox("Histogram"):
+                plot_numerical(df_disc, variable, 'SalePrice', hue_order)
+            if st.checkbox("Lineplot"):
+                plot_line(df, variable, 'SalePrice')
 
-    
+
 def print_conclusions(cols_and_conclusions):
     conclusions = ""
     for conclusion in list(cols_and_conclusions.values()):
         conclusions += f"* {conclusion}\n"
 
-    return conclusions   
-    
+    return conclusions
+
 
 def plot_numerical(df, col, target_var, hue_order):
     fig, axes = plt.subplots(figsize=(8, 5))
-    sns.histplot(data=df, x=col, hue=target_var, hue_order=hue_order,kde=True,element="step") 
-    plt.title(f"{col}", fontsize=20,y=1.05)
+    sns.histplot(data=df, x=col, hue=target_var,
+                 hue_order=hue_order, kde=True, element="step")
+    plt.title(f"{col}", fontsize=20, y=1.05)
+    st.pyplot(fig)
+
+
+def plot_line(df, col, target_var):
+    fig, axes = plt.subplots(figsize=(8, 5))
+    sns.lineplot(data=df, x=target_var, y=col, color='#FC6C85')
+    plt.title(f"{col}", fontsize=20, y=1.05)
     st.pyplot(fig)
 
 
 def sale_price_discretisation(df):
 
     # disctretises 'SalePrice'
-    discretiser= EqualFrequencyDiscretiser(q=6, variables=['SalePrice'])
+    discretiser = EqualFrequencyDiscretiser(q=6, variables=['SalePrice'])
     discretiser.fit(df)
-    df_disc=discretiser.transform(df)
-    
-    # maps quantile values to 'SalePrice'
-    labels=discretiser.binner_dict_['SalePrice']
-    q_value= len(labels)-1
-    labels_map={}
+    df_disc = discretiser.transform(df)
 
-    for x in range(0,q_value):
+    # maps quantile values to 'SalePrice'
+    labels = discretiser.binner_dict_['SalePrice']
+    q_value = len(labels)-1
+    labels_map = {}
+
+    for x in range(0, q_value):
         if x == 0:
             labels_map[x] = f"< {int(labels[1])}"
-        elif x < q_value -1:
+        elif x < q_value - 1:
             labels_map[x] = f"{int(labels[x])} - {int(labels[x+1])}"
         else:
-            labels_map[x] =f"{int(labels[x])} +"
-    
+            labels_map[x] = f"{int(labels[x])} +"
+
     df_disc["SalePrice"] = df_disc["SalePrice"].replace(labels_map)
 
     # creates hue_order for histograms
     hue_order = list(labels_map.values())
-  
+
     return df_disc, hue_order
 
 
@@ -157,49 +165,41 @@ def calculate_corr_and_pps(df):
     df_corr_spearman = df.corr(method="spearman")
     df_corr_pearson = df.corr(method="pearson")
     pps_matrix_raw = pps.matrix(df)
-    pps_matrix = pps_matrix_raw.filter(['x', 'y', 'ppscore']).pivot(columns='x', index='y', values='ppscore')
-    
+    pps_matrix = pps_matrix_raw.filter(['x', 'y', 'ppscore']).pivot(
+        columns='x', index='y', values='ppscore')
+
     return df_corr_pearson, df_corr_spearman, pps_matrix
 
 
-def heatmap_corr(df,threshold, figsize=(20,14), font_annot = 14):
-  if len(df.columns) > 1:
-    mask = np.zeros_like(df, dtype=np.bool)
-    mask[np.triu_indices_from(mask)] = True
-    mask[abs(df) < threshold] = True
+def heatmap_corr(df, threshold, figsize=(20, 14), font_annot=14):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
 
-    fig, axes = plt.subplots(figsize=figsize)
-    sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
-                mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
-                linewidth=0.5, 
-                     )
-    axes.set_yticklabels(df.columns, rotation = 0, fontsize =20)
-    axes.set_xticklabels(df.columns, fontsize =20)
-    plt.ylim(len(df.columns),0)
-    st.pyplot(fig)
+        fig, axes = plt.subplots(figsize=figsize)
+        sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                    mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
+                    linewidth=0.5,
+                    )
+        axes.set_yticklabels(df.columns, rotation=0, fontsize=20)
+        axes.set_xticklabels(df.columns, fontsize=20)
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
 
 
-def heatmap_pps(df,threshold, figsize=(20,12), font_annot = 8):
+def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
     if len(df.columns) > 1:
 
         mask = np.zeros_like(df, dtype=np.bool)
         mask[abs(df) < threshold] = True
 
         fig, ax = plt.subplots(figsize=figsize)
-        ax = sns.heatmap(df, annot=True, xticklabels=True,yticklabels=True,
-                        mask=mask,cmap='rocket_r', annot_kws={"size": font_annot},
-                        linewidth=0.05,linecolor='grey')
-        ax.set_yticklabels(df.columns, rotation = 0, fontsize =20)
-        ax.set_xticklabels(df.columns, fontsize =20)
+        ax = sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                         mask=mask, cmap='rocket_r', annot_kws={"size": font_annot},
+                         linewidth=0.05, linecolor='grey')
+        ax.set_yticklabels(df.columns, rotation=0, fontsize=20)
+        ax.set_xticklabels(df.columns, fontsize=20)
 
-        plt.ylim(len(df.columns),0)
+        plt.ylim(len(df.columns), 0)
         st.pyplot(fig)
-
-
-
-
-
-
-
-
-
